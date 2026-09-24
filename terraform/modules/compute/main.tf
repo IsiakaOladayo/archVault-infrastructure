cbc736693f0b5b1938911bdebf7b15310deaace2
ikigai-crypto <eodekunle52@gmail.com>
2026-09-24 13:27:35 +0100

    Replace IAM role ARNs with variable references

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -56,8 +56,8 @@ resource "aws_ecs_task_definition" "application" {
   cpu    = var.cpu
   memory = var.memory
 
-  execution_role_arn = aws_iam_role.ecs_task_execution.arn
-  task_role_arn      = aws_iam_role.ecs_task.arn
+  execution_role_arn = var.ecs_execution_role_arn
+  task_role_arn      = var.ecs_task_role_arn
 
   container_definitions = jsonencode([
     {
