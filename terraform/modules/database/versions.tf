a359dee78e1b30b6e28c70411c9c461682b411bc
ikigai-crypto <eodekunle52@gmail.com>
2026-09-24 11:40:51 +0100

    Update versions.tf

--- terraform/modules/database/versions.tf
+++ terraform/modules/database/versions.tf
@@ -1,12 +1,11 @@
 terraform {
+  required_version = ">= 1.6.0"
+
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-
-      configuration_aliases = [
-        aws.primary,
-        aws.dr
-      ]
+      source                = "hashicorp/aws"
+      version               = "~> 6.0"
+      configuration_aliases = [aws.primary, aws.dr]
     }
   }
 }
